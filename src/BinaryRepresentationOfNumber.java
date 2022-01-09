205889040208fc47f03d1334494abaf3dde157b4
dupa1232 <misiu889@interia.pl>
2022-01-09 17:28:09 +0100

    get binary, hex, octa representation of number

---
+++ src/BinaryRepresentationOfNumber.java
@@ -0,0 +1,19 @@
+public class BinaryRepresentationOfNumber {
+    public static void main(String[] args) {
+        int a = 525;
+        long b = 12456545645L;
+
+        String binaryA = Integer.toString(a, 2);
+        System.out.println("Binary representation"
+                + " of A : " + binaryA);
+        String binaryB = Long.toString(b, 2);
+        System.out.println("Binary representation"
+                + " of B : " + binaryB);
+        String octalA = Integer.toString(a, 8);
+        System.out.println("Octal representation"
+                + " of A : " + octalA);
+        String octalB = Long.toString(b, 8);
+        System.out.println("Octal representation"
+                + " of B : " + octalB);
+    }
+}
