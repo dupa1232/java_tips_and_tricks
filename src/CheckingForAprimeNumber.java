be1589bd8ba34d03ae58391b4f599d3d3aaa2f79
dupa1232 <misiu889@interia.pl>
2022-01-09 17:11:22 +0100

    checking for a prime number

---
+++ src/CheckingForAprimeNumber.java
@@ -0,0 +1,7 @@
+import java.math.BigInteger;
+
+public class CheckingForAprimeNumber {
+    public static void main(String[] args) {
+        System.out.println(BigInteger.valueOf(1235).isProbablePrime(1) );
+    }
+}
